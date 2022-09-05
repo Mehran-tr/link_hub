9288b623ce011df280bab8935280839fb87bea26
Mohammad <usermp76@gmail.com>
2022-09-05 17:30:45 +0430

    add fillables and soft delete

--- app/Models/LinkCategoryRelation.php
+++ app/Models/LinkCategoryRelation.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LinkCategoryRelation extends Model
 {
-    use HasFactory;
+    use HasFactory , SoftDeletes;
+
+    protected $fillable = [
+        "link_id",
+        "category_id"
+    ];
+    protected $hidden = [
+        "created_at",
+        "updated_at"
+    ];
 }
